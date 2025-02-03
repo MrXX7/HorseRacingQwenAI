852261ea02443ea1c675a9165d91c298acdbd352
Oncu <oohancan@gmail.com>
2025-02-03 23:19:02 +0300

    Initial Commit

---
+++ HorseRacing/HorseRacingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HorseRacingApp.swift
+//  HorseRacing
+//
+//  Created by Oncu Can on 3.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct HorseRacingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
